521ea55cf021322217fb9bcc7b3db548bf0baa80
nordine.chaumath@hotmail.com <nordine.chaumath@hotmail.com>
2022-02-06 14:20:33 -0800

    made main class to seperate the main method from the Maze class

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nordine
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Maze.java
+++ src/Maze.java
@@ -8,12 +8,5 @@
  */
 public class Maze {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
