48a2e08c2ed314207d9043c2be172ecd6c4673da
nordine.chaumath@hotmail.com <nordine.chaumath@hotmail.com>
2022-02-13 19:15:53 -0800

    added a valid path algorithm, updated Room, added an additional constraint

--- src/Main.java
+++ src/Main.java
@@ -13,43 +13,26 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		final Scanner myInput = new Scanner(System.in);
-		Maze maze;
-		String rand;
+		Maze maze = null;
 		int myRows;
 		int myCols;
-		int myX;
-		int myY;
-		int myEndX;
-		int myEndY;
 		
-		System.out.print("Maze randomize (y or n)? ");
-		rand = myInput.next();
-		if(rand.toLowerCase().equals("n")) {
-			//will add valid input condition later
-			System.out.print("Input amount of rows(between 4 and 10): ");
-			myRows = myInput.nextInt();
-			System.out.print("Input amount of cols(between 4 and 10): ");
-			myCols = myInput.nextInt();
-			System.out.print("Input starting row: ");
-			myX = myInput.nextInt();
-			System.out.print("Input starting col: ");
-			myY = myInput.nextInt();
-			System.out.print("Input ending row: ");
-			myEndX = myInput.nextInt();
-			System.out.print("Input ending col: ");
-			myEndY = myInput.nextInt();
-			
-			maze = new Maze(myRows, myCols, myX, myY, myEndX, myEndY);
-//			maze = new Maze(4, 7, 0, 0, 3, 3);
-		}else {
-			maze = new Maze();
-		}
+		//will add valid input condition later
+		System.out.print("Input amount of rows(between 4 and 10): ");
+		myRows = myInput.nextInt();
+		System.out.print("Input amount of cols(between 4 and 10): ");
+		myCols = myInput.nextInt();
 		
+		maze = new Maze(myRows, myCols);
 		
+//		maze.lockRoom(2, 1);
+		maze.lockRoom(1, 2);
 		
 		maze.displayMaze();
 		
 		System.out.println(maze.availableRoom());
+		System.out.print("Valid path? ");
+		System.out.print(maze.hasPath());
 		
 		myInput.close();
 	}
