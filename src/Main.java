cbc9490d0edde21c5ca82e9d24f3fbe9a01510e7
Brianna Crowley <crowleyb@uw.edu>
2022-02-21 22:39:03 -0800

    Brianna Crowley commit, the main menu has been updated with the placeholder menu being commented out, and the menu itself having the options of a new game, a load game option that does not function yet, a help menu to teach the player the instructions on how to play the game, a cheats menu that isn't implmented yet, and an exit option that exits the game.

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		final Scanner myInput = new Scanner(System.in);
 		Maze maze = null;
@@ -39,6 +39,59 @@ public class Main {
 
 		
 		myInput.close();
-	}
+	} */
 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final Scanner myInput = new Scanner(System.in);
+		Maze maze = null;
+		int myRows;
+		int myCols;
+		boolean myGameDone = false;
+		
+		while (!myGameDone) {
+			System.out.println("Hello, welcome to the Trivia Maze, please type in" +
+								" a valid number to select your options:");
+			System.out.println("1. New Game");
+			System.out.println("2. Load Saved Game");
+			System.out.println("3. Help");
+			System.out.println("4. Cheats Menu");
+			System.out.println("5. Exit");
+			
+			if (myInput.nextInt() == 1) {
+				//put the original main code here or call a helper method that does that
+			}
+			
+			else if (myInput.nextInt() == 2) {
+				//loading and saving games are not implemented yet.
+			}
+			
+			else if (myInput.nextInt() == 3) {
+				//This is where a series of instructions are printed onto the console
+				//before sending you back to the main menu options.
+			}
+			
+			else if (myInput.nextInt() == 4) {
+				//This is where a text-based cheats menu would be
+				System.out.println("You picked the cheats menu when there's nothing here!");
+			}
+			
+			else if (myInput.nextInt() == 5) {
+				myGameDone = true;
+			}
+			
+			else {
+				System.out.print("Invalid input, please try again.");
+				System.out.print("1. New Game");
+				System.out.print("2. Load Saved Game");
+				System.out.print("3. Help");
+				System.out.print("4. Cheats Menu");
+				System.out.print("5. Exit");
+			}
+		}
+		
+		
+	}
 }
