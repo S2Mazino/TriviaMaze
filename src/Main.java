4b51b1d0aed62396846e259eb3e7c947fb727348
Brianna Crowley <crowleyb@uw.edu>
2022-02-22 19:59:32 -0800

    Brianna Crowley commit, editing and updating the Help Menu's first draft, working on the ability to move forward and back from pages and provide more clarity to the instructions.

--- src/Main.java
+++ src/Main.java
@@ -71,23 +71,22 @@ public class Main {
 			else if (myInput.nextInt() == 3) {
 				//This is where a series of instructions are printed onto the console
 				//before sending you back to the main menu options.
-				System.out.println("To play the Trivia Maze, you can select a New Game" +
-						" to start a new game file and start playing the maze.");
-				System.out.println("The first number you enter when starting a new game" +
-						"determines the number of rows your maze has.");
-				System.out.println("The second number you enter when starting a new game" +
-						"determines the number of columns your maze has.");
-				System.out.println("The Trivia Maze will be created based on the parameters" +
-						"And the goal is the reach the End Room to wind the game!");
-				System.out.println("Press Enter to continue.");
-				if (myInput.next() == " ") {
-					System.out.println("The starting point is located at the top-right corner.");
-					System.out.println("The exit is located at the farthest bottom-left corner.");
-					System.out.println("You are represented by the 'u' avatar! You can move this " +
-										"avatar through doors.");
-					System.out.println("Press the N key for moving up, press the S key to move down.");
-					System.out.println("Press the E key for moving left, press the W key to move right.");
-					System.out.println("Press Enter to continue. Press B to go back to the previous page.");
+				displayOpeningInstr();
+				System.out.println("Press N to continue.");
+				if (myInput.next() == "N") {
+					displayPlayerInstr();
+					System.out.println("Press N to continue. Press B to go back to the previous page.");
+					if (myInput.next() == "N") {
+						displayDoorInstr();
+						System.out.println("Press N to continue. Press B to go back to the previous page.");
+						if (myInput.next() == "N") {
+							displaySaveLoadInstr();
+							System.out.println("Press N to go back to the main menu. Press B to go back to the previous page.");
+							if (myInput.next() == "N") {
+								//get out of this loop
+							}
+						}
+					}
 				}
 			}
 			
@@ -110,6 +109,43 @@ public class Main {
 			}
 		}
 		
-		
+	}
+	
+	public static void displayPlayerInstr() {
+		System.out.println("The starting point is located at the top-right corner.");
+		System.out.println("The exit is located at the farthest bottom-left corner.");
+		System.out.println("You are represented by the 'u' avatar! You can move this " +
+							"avatar through doors.");
+		System.out.println("Press the N key for moving up, press the S key to move down.");
+		System.out.println("Press the E key for moving left, press the W key to move right.");
+	}
+	
+	public static void displayDoorInstr() {
+		System.out.println("When you go through a door (represented by a 'o') you will be asked a question!");
+		System.out.println("Select your answer with the shown key.");
+		System.out.println("If you select the right answer, you will go through the door and into the room.");
+		System.out.println("Select the wrong answer and the door will be blocked (represented by an 'x') and "
+							+ "you will be unable to pass through the door. These doors cannot be opened again!");
+		System.out.println("If you cannot get to the exit or you become entrapped in a room, it's game over!");
+	}
+	
+	public static void displayOpeningInstr() {
+		System.out.println("To play the Trivia Maze, you can select a New Game" +
+				" to start a new game file and start playing the maze.");
+		System.out.println("The first number you enter when starting a new game" +
+				"determines the number of rows your maze has.");
+		System.out.println("The second number you enter when starting a new game" +
+				"determines the number of columns your maze has.");
+		System.out.println("The Trivia Maze will be created based on the parameters" +
+				"And the goal is the reach the End Room to wind the game!");
+	}
+	
+	public static void displaySaveLoadInstr() {
+		System.out.println("To save a current game, while playing the game, press the" +
+				"'F' key in order to save your progress..");
+		System.out.println("To load your last save, go back to the main menu" +
+				"and select Load Saved Game.");
+		System.out.println("While playing the game, press the 'X' key to leave the current game" +
+				" without saving your progress.");
 	}
 }
